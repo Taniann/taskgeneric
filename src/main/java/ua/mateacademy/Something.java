2cb32f1fcfce71dc2990af3e5969bb0eada168cd
Таня <nebesnatania@outlook.com>
2019-01-24 13:14:57 +0200

    smth

---
+++ src/main/java/ua/mateacademy/Something.java
@@ -0,0 +1,10 @@
+package ua.mateacademy;
+
+/**
+ * Created by Tania Nebesna on 24.01.2019.
+ */
+public class Something {
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+}
